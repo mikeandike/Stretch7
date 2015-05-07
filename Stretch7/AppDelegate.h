cf378a5e1277648678e48a0736b19b178a3f1ade
Michael Sacks <michaelsacks@gmail.com>
2015-05-07 09:30:33 -0600

    Initial Commit

---
+++ Stretch7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stretch7
+//
+//  Created by Michael Sacks on 5/7/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stretch7/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Stretch7
+//
+//  Created by Michael Sacks on 5/7/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
